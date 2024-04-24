7b0174b2d14f5ccb4a9eb30d21809edc2c878572
Dharitri-org <rewa.volga@gmail.com>
2024-04-25 01:42:20 +0530

    v0.0.7

--- data/transaction/log.go
+++ data/transaction/log.go
@@ -1,4 +1,4 @@
-//go:generate protoc -I=proto -I=$GOPATH/src -I=$GOPATH/src/github.com/numbatx/protobuf/protobuf  --gogoslick_out=. log.proto
+//go:generate protoc -I=. -I=$GOPATH/src -I=$GOPATH/src/github.com/numbatx/protobuf/protobuf  --gogoslick_out=. log.proto
 package transaction
 
 import (
